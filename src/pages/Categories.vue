a23a7bdf787d1a310a99e648f7af65fe3d781c20
AnnaKoha <kohovich.anna@gmail.com>
2025-07-25 15:53:13 +0300

    add categories

---
+++ src/pages/Categories.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { useRootStore } from '@/stores/root'
+import AppLayout from '@/layouts/AppLayout.vue';
+import AppButton from '@/components/AppButton.vue';
+
+const rootStore = useRootStore();
+const categories = computed(() => rootStore.categories);
+
+</script>
+
+<template>
+    <AppLayout>
+        <template #title>Категории</template>
+        <template #controls>
+            <!-- <AppButton text="Добавить категорию"></AppButton> -->
+        </template>
+        
+        <template #inner>
+            <el-table :data="categories" style="width: 100%">
+                    <el-table-column prop="strCategory" label="Category"/>
+            </el-table>
+        </template>
+    </AppLayout>
+</template>
